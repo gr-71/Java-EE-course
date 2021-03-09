2c5c9aa4ac80788e954fbbda10e05b0e2f4f5989
grybentsov <grybentsov@gmail.com>
2021-03-09 15:29:26 +0300

    hw-to-lesson-6

--- first-jsf-app/src/main/java/ru/geekbrains/service/CartServiceImpl.java
+++ first-jsf-app/src/main/java/ru/geekbrains/service/CartServiceImpl.java
@@ -1,17 +1,28 @@
 package ru.geekbrains.service;
 
 
+import javax.ejb.EJB;
 import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
 import java.util.HashMap;
 import java.util.Map;
 
 @Stateful
 public class CartServiceImpl implements CartService {
 
+    @EJB
     private final Map<Long, ProductRepr> productMap = new HashMap<>();
 
+    @TransactionAttribute
     @Override
     public void addToCart(ProductRepr product) {
-
+        productMap.put(product.getId(), product);
     }
+
+    @TransactionAttribute
+    @Override
+    public void removeFromCart(ProductRepr product) {
+        productMap.remove(product.getId());
+    }
+
 }
